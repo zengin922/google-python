a62a6fabfb4398a8a307fadefe40b03193399a1c
zengin922 <zengin92@live.com>
2026-04-10 01:21:50 +0100

    Initial commit

--- week1_basics/hello.py
+++ week1_basics/hello.py
@@ -1,1 +1,10 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+
+print("Hello " + name)
+print("You are " + str(age) + " years old")
+
+print("In 5 years you will be " + str(age + 5) + " years old" )
+
 
