6226bdbe31640e6b74b65d6e38386f665dfab4d4
zengin922 <zengin92@live.com>
2026-04-10 01:25:15 +0100

    Day 1 - User input & age calculation

--- week1_basics/hello.py
+++ week1_basics/hello.py
@@ -5,5 +5,6 @@ age = int(input("How old are you? "))
 print("Hello " + name)
 print("You are " + str(age) + " years old")
 
-print("In 5 years you will be " + str(age + 5) + " years old" )
+print("In 6 years you will be " + str(age + 6) + " years old" )
+
 
