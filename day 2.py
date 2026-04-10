8c32cfff12cd2a6a335a83ebf27d61649b7d57e2
zengin922 <zengin92@live.com>
2026-04-10 11:27:17 +0100

    Day 1 - User input & age calculation

--- day 2.py
+++ day 2.py
@@ -1,1 +1,4 @@
+for i in range(5):
+    print("This is fun!")
 
+## Very interesting start, really enjoying the use of different colour schemes on Pycharm. This piece of code prints out This is fun! 5 times displayed on the terminal output.
